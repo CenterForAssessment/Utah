3ca3804475abab3c9a570dfa0c22c1d75671d0f2
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-08-02 09:12:02 -0500

    LONG data file cleaning needed to run 2013 analyses

--- Utah_Data_LONG.R
+++ Utah_Data_LONG.R
@@ -5,118 +5,36 @@
 ##########################################################
 
 library(SGP)
-require(car)
+library(data.table)
 
-library(foreign)
+### Set working directory
 
-# Set working directory
-setwd("C:/Utah_SGP_100512/Data")
+### Read in USOE .Rdata file
+load("Utah_Data_LONG.Rdata")
 
-### Read in SPSS file
-Utah_Data_LONG <- read.spss("C:/Utah_SGP_100512/Data/Utah_Data_LONG.sav", to.data.frame = TRUE, use.value.labels = TRUE)
+### Final cleaning needed for File Andrew uploaded 7/31/13
 
-gc()
+## Convert 8th grade math to Pre-Algebra
+Utah_Data_LONG <- data.table(Utah_Data_LONG)
+Utah_Data_LONG[which(CONTENT_AREA == 'MATHEMATICS' & GRADE == 8 & YEAR == 2013), GRADE := 'EOCT']
+Utah_Data_LONG[which(CONTENT_AREA == 'MATHEMATICS' & GRADE == 'EOCT' & YEAR == 2013), CONTENT_AREA := 'PRE_ALGEBRA']
+table(Utah_Data_LONG$GRADE, Utah_Data_LONG$CONTENT_AREA, Utah_Data_LONG$YEAR, Utah_Data_LONG$VALID_CASE)
 
-Utah_Data_LONG=data.frame(Utah_Data_LONG)
-
-#Trim white space from SPSS
-
-trimWhiteSpace <- function(line) gsub("(^ +)|( +$)", "", line)
-
-Utah_Data_LONG[['FIRST_NAME']]<- trimWhiteSpace(Utah_Data_LONG[['FIRST_NAME']])
-Utah_Data_LONG[['LAST_NAME']] <- trimWhiteSpace(Utah_Data_LONG[['LAST_NAME']])
-Utah_Data_LONG[['VALID_CASE']] <- trimWhiteSpace(Utah_Data_LONG[['VALID_CASE']])
-Utah_Data_LONG[['ACHIEVEMENT_LEVEL']] <- trimWhiteSpace(Utah_Data_LONG[['ACHIEVEMENT_LEVEL']])
-Utah_Data_LONG[['YEAR']] <- trimWhiteSpace(Utah_Data_LONG[['YEAR']])
-Utah_Data_LONG[['ID']] <- trimWhiteSpace(Utah_Data_LONG[['ID']])
-Utah_Data_LONG[['SCHOOL_NUMBER']] <- trimWhiteSpace(Utah_Data_LONG[['SCHOOL_NUMBER']])
-Utah_Data_LONG[['SCALE_SCORE']] <- trimWhiteSpace(Utah_Data_LONG[['SCALE_SCORE']])
-Utah_Data_LONG[['CONTENT_AREA']] <- trimWhiteSpace(Utah_Data_LONG[['CONTENT_AREA']])
-Utah_Data_LONG[['SCHOOL_NAME']] <- trimWhiteSpace(Utah_Data_LONG[['SCHOOL_NAME']])
-Utah_Data_LONG[['DISTRICT_NAME']] <- trimWhiteSpace(Utah_Data_LONG[['DISTRICT_NAME']])
-Utah_Data_LONG[['DISTRICT_NUMBER']] <- trimWhiteSpace(Utah_Data_LONG[['DISTRICT_NUMBER']])
-Utah_Data_LONG[['EMH_LEVEL']] <- trimWhiteSpace(Utah_Data_LONG[['EMH_LEVEL']])
-Utah_Data_LONG[['DISTRICT_ENROLLMENT_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['DISTRICT_ENROLLMENT_STATUS']])
-Utah_Data_LONG[['SCHOOL_ENROLLMENT_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['SCHOOL_ENROLLMENT_STATUS']])
-Utah_Data_LONG[['STATE_ENROLLMENT_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['STATE_ENROLLMENT_STATUS']])
-Utah_Data_LONG[['SUBGROUP']] <- trimWhiteSpace(Utah_Data_LONG[['SUBGROUP']])
-Utah_Data_LONG[['TEST_NAME']] <- trimWhiteSpace(Utah_Data_LONG[['TEST_NAME']])
-Utah_Data_LONG[['GENDER']] <- trimWhiteSpace(Utah_Data_LONG[['GENDER']])
-Utah_Data_LONG[['SUBGROUP']] <- trimWhiteSpace(Utah_Data_LONG[['SUBGROUP']])
-Utah_Data_LONG[['ETHNICITY']] <- trimWhiteSpace(Utah_Data_LONG[['ETHNICITY']])
-Utah_Data_LONG[['FRL_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['FRL_STATUS']])
-Utah_Data_LONG[['IEP_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['IEP_STATUS']])
-Utah_Data_LONG[['ELL_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['ELL_STATUS']])
-Utah_Data_LONG[['GT_STATUS']] <- trimWhiteSpace(Utah_Data_LONG[['GT_STATUS']])
-Utah_Data_LONG[['TEST_MEDIUM']] <- trimWhiteSpace(Utah_Data_LONG[['TEST_MEDIUM']])
-
-### Clean up the longfile - Specify classes
-
-Utah_Data_LONG$SCALE_SCORE <- as.integer(Utah_Data_LONG$SCALE_SCORE)
-
-Utah_Data_LONG$ACHIEVEMENT_LEVEL <- factor(Utah_Data_LONG$ACHIEVEMENT_LEVEL, levels=1:4, 
-	labels=c("Level 1", "Level 2", "Level 3", "Level 4"), ordered=TRUE)
-
-Utah_Data_LONG$STATE_ENROLLMENT_STATUS <- factor(Utah_Data_LONG$STATE_ENROLLMENT_STATUS)
-
-Utah_Data_LONG$DISTRICT_ENROLLMENT_STATUS <- factor(Utah_Data_LONG$DISTRICT_ENROLLMENT_STATUS)
-
-Utah_Data_LONG$SCHOOL_ENROLLMENT_STATUS <- factor(Utah_Data_LONG$SCHOOL_ENROLLMENT_STATUS)
-
-Utah_Data_LONG$SUBGROUP <- factor(Utah_Data_LONG$SUBGROUP)
-levels(Utah_Data_LONG$SUBGROUP) <- c("No", "Yes")
-
-Utah_Data_LONG$GENDER <- factor(Utah_Data_LONG$GENDER)
-levels(Utah_Data_LONG$GENDER) <- c("F", "M")
-
-Utah_Data_LONG$FRL_STATUS <- factor(Utah_Data_LONG$FRL_STATUS)
-levels(Utah_Data_LONG$FRL_STATUS) <- c("No", "Yes")
-
-Utah_Data_LONG$IEP_STATUS <- factor(Utah_Data_LONG$IEP_STATUS)
-levels(Utah_Data_LONG$IEP_STATUS) <- c("No", "Yes")
-
-Utah_Data_LONG$ELL_STATUS <- factor(Utah_Data_LONG$ELL_STATUS)
-levels(Utah_Data_LONG$ELL_STATUS) <- c("No", "Yes")
-
-Utah_Data_LONG$GT_STATUS <- factor(Utah_Data_LONG$GT_STATUS)
-levels(Utah_Data_LONG$GT_STATUS) <- c("No", "Yes")
+##  All 0s appear to be Algebra II scores of 0.  All are INVALID CASES too
+Utah_Data_LONG$ACHIEVEMENT_LEVEL <- factor(Utah_Data_LONG$ACHIEVEMENT_LEVEL, levels=0:4, 
+	labels=c(NA, "Level 1", "Level 2", "Level 3", "Level 4"), ordered=TRUE)
 
 Utah_Data_LONG$ACHIEVEMENT_LEVEL_FULL <- Utah_Data_LONG$ACHIEVEMENT_LEVEL
-levels(Utah_Data_LONG$ACHIEVEMENT_LEVEL) <- c("BP", "BP", "P", "A")
 
-Utah_Data_LONG$TEST_MEDIUM <- factor(Utah_Data_LONG$TEST_MEDIUM)
-levels(Utah_Data_LONG$TEST_MEDIUM) <- c("CRT", "NWEA", "UAA")
+levels(Utah_Data_LONG$ACHIEVEMENT_LEVEL) <- c(NA, "BP", "BP", "P", "A")
 
-Utah_Data_LONG$BP <- NULL
+table(Utah_Data_LONG$ACHIEVEMENT_LEVEL, exclude=NULL)
+table(Utah_Data_LONG$ACHIEVEMENT_LEVEL_FULL, exclude=NULL)
 
-Utah_Data_LONG$ETHNICITY <- factor(Utah_Data_LONG$ETHNICITY)
-levels(Utah_Data_LONG$ETHNICITY) <- c("African American", "American Indian", "Asian", "Hispanic/Latino", "Multiple Races", "Pacific Islander", "Unknown", "White")
+###  Clean up the ETHNICITY Lables
+levels(Utah_Data_LONG$ETHNICITY) <- c("African American", "African American", "American Indian", "Asian", "White", "Hispanic/Latino", "Hispanic/Latino", "Multiple Races", "Pacific Islander", "Unknown", "White")
 
-
-###
-###		Changes to UT_Data_LONG needed to run EOCT course specific progression analyses
-###
-
-##  Change CONTENT_AREA to reflect EOCT course info
-
-Utah_Data_LONG$CONTENT_AREA <- as.character(Utah_Data_LONG$CONTENT_AREA)
-
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Pre-Algebra'] <- 'PRE_ALGEBRA'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Algebra I'] <- 'ALGEBRA_I'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Algebra II'] <- 'ALGEBRA_II'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Geometry'] <- 'GEOMETRY'
-
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Biology'] <- 'BIOLOGY'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Chemistry'] <- 'CHEMISTRY'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Earth Systems Science'] <- 'EARTH_SCIENCE'
-Utah_Data_LONG$CONTENT_AREA[Utah_Data_LONG$TEST_NAME == 'Physics'] <- 'PHYSICS'
-
-## Keep a copy of the actual grade since we'll be changing this again for 2012 analyses.
-Utah_Data_LONG$GRADE_REPORTED <- Utah_Data_LONG$GRADE
-
-###  Change values of GRADE to 'EOCT'
-Utah_Data_LONG$GRADE[!Utah_Data_LONG$CONTENT_AREA %in% c('ELA', 'MATHEMATICS', 'SCIENCE')] <- 'EOCT'
-
-save(Utah_Data_LONG, file="Utah_Data_LONG.Rdata")
+###  Save data object.
+save(Utah_Data_LONG, file="Utah_Data_LONG-2013_FINAL.Rdata")
 
 
