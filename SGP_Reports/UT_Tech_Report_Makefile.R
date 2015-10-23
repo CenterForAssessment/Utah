a21e370360c165b9696f41661f088e5630c68de9
Adam VanIwaarden <avaniwaarden@gmail.com>
2015-10-22 22:00:36 -0600

    First commit of Utah SGP documentation files.

---
+++ SGP_Reports/UT_Tech_Report_Makefile.R
@@ -0,0 +1,87 @@
+
+#######
+# 2014
+#######
+
+load("/Users/avi/Dropbox/SGP/Utah_SGP.Rdata")
+setwd("/Users/avi/Dropbox/Github_Repos/Documentation/Utah/SGP_Reports/2014")
+
+library(SGPreports)
+use.data.table()
+source('~/Dropbox (CenterforAssessment)/Github_Repos/Packages/SGPreports/R/renderEPUB.R')
+
+renderMultiDocument(rmd_input = "Utah_SGP_Report_2014.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "EPUB", "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE, 
+										cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+renderMultiDocument(rmd_input = "Appendix_A_2014.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "EPUB", "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE)#,
+										# cleanup_aux_files = FALSE)
+
+
+renderMultiDocument(rmd_input = "Appendix_B.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE,
+										# self_contained = FALSE,
+										# cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+renderMultiDocument(rmd_input = "Appendix_C.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "EPUB", "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE, 
+										# cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+
+#######
+# 2015
+#######
+
+# load("/Users/avi/Dropbox/SGP/Utah/Data/Utah_SGP.Rdata")
+load("/Users/avi/Dropbox/SGP/Utah_SGP_FIN.Rdata")
+setwd("/Users/avi/Dropbox/Github_Repos/Documentation/Utah/SGP_Reports/2015")
+
+###  Clean up the GoFit Plot directories
+
+# gof <- "/Users/avi/Dropbox/Github_Repos/Documentation/Utah/SGP_Reports/img/Goodness_of_Fit"
+# tmp_files <- grep("2014", list.files(gof), value = TRUE)
+# tmp_files <- grep("2015", list.files(gof), value = TRUE)
+# 
+# for (k in tmp_files) {
+# 	tmp_rm <- grep(".pdf", list.files(paste(gof, k, sep="/")), value = TRUE)
+# 	if (length(tmp_rm) > 0) unlink(paste(gof, k, tmp_rm, sep="/"))
+# }
+
+
+library(SGPreports)
+use.data.table()
+# source('~/Dropbox (CenterforAssessment)/Github_Repos/Packages/SGPreports/R/renderEPUB.R')
+
+renderMultiDocument(rmd_input = "Utah_SGP_Report_2015.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "EPUB", "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE, 
+										#cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
+
+renderMultiDocument(rmd_input = "Appendix_A_2015.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "EPUB", "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE)#,
+										# cleanup_aux_files = FALSE)
+
+
+renderMultiDocument(rmd_input = "Appendix_B.Rmd",
+										output_format = c("HTML", "EPUB", "PDF"), #, "DOCX"
+										cover_img="../img/cover.jpg",
+										add_cover_title=TRUE,
+										# self_contained = FALSE,
+										# cleanup_aux_files = FALSE,
+										pandoc_args = "--webtex")
