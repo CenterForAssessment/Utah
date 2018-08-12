fede72b0357ebfdcfd0d761c53e037e5a81ab461
Adam VanIwaarden <avaniwaarden@gmail.com>
2018-08-12 17:04:37 -0600

    Course progression analysis and changes to configs

--- SGP_CONFIG/EOCT/2017/SCIENCE.R
+++ SGP_CONFIG/EOCT/2017/SCIENCE.R
@@ -1,97 +1,60 @@
-#############################################################
-# EOCT CONFIGURATION FILE - HIGH SCHOOL SCIENCE
-#############################################################
+###########################################################################
+###                                                                     ###
+###      Utah EOCT CONFIGURATION FILE  -  2018 HIGH SCHOOL SCIENCE      ###
+###                                                                     ###
+###########################################################################
+
 	EARTH_SCIENCE_2017.config <- list(
 		EARTH_SCIENCE.2017 = list(
 			sgp.content.areas=c(rep('SCIENCE', 3), 'EARTH_SCIENCE'),
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(6:8, 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-			sgp.norm.group.preference=0),
-		EARTH_SCIENCE.2017 = list(
-		  sgp.content.areas=c(rep('SCIENCE', 2), 'EARTH_SCIENCE'),
-		  sgp.panel.years=as.character(2015:2017),
-		  sgp.grade.sequences=list(c(7:8, 'EOCT')),
-		  sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-		  sgp.norm.group.preference=1),
-		EARTH_SCIENCE.2017 = list(
-			sgp.content.areas=c('SCIENCE', 'EARTH_SCIENCE'),
-			sgp.panel.years=as.character(2016:2017),
-			sgp.grade.sequences=list(c(8, 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.norm.group.preference=2)
+			sgp.norm.group.preference=0)
 	)
+
 #############################################################
+
 	BIOLOGY_2017.config <- list(
-	# REPEATER
+		# REPEATER
 		BIOLOGY.2017 = list(
 			sgp.content.areas=c('BIOLOGY', 'BIOLOGY'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
 			sgp.norm.group.preference=0),
-	# VIA GRADE 8
+
+		# VIA GRADE 8
 		BIOLOGY.2017 = list(
 			sgp.content.areas=c(rep('SCIENCE', 3), 'BIOLOGY'),
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(6:8, 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="SCIENCE_BIO",
 			sgp.norm.group.preference=1),
-	  BIOLOGY.2017 = list(
-	    sgp.content.areas=c(rep('SCIENCE', 2), 'BIOLOGY'),
-	    sgp.panel.years=as.character(2015:2017),
-	    sgp.grade.sequences=list(c(7:8, 'EOCT')),
-	    sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-	    sgp.norm.group.preference=2),
-		BIOLOGY.2017 = list(
-			sgp.content.areas=c('SCIENCE', 'BIOLOGY'),
-			sgp.panel.years=as.character(2016:2017),
-			sgp.grade.sequences=list(c(8, 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.norm.group.preference=3,
-			sgp.projection.sequence = "SCIENCE_BIO"),
-	# VIA EARTH SCIENCE
+
+	# CANONICAL - VIA EARTH SCIENCE
 		BIOLOGY.2017 = list(
 			sgp.content.areas=c(rep('SCIENCE', 2), 'EARTH_SCIENCE', 'BIOLOGY'),
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(7:8, 'EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-			sgp.norm.group.preference=1),
-	  BIOLOGY.2017 = list(
-	    sgp.content.areas=c('SCIENCE', 'EARTH_SCIENCE', 'BIOLOGY'),
-	    sgp.panel.years=as.character(2015:2017),
-	    sgp.grade.sequences=list(c(8, 'EOCT', 'EOCT')),
-	    sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-	    sgp.norm.group.preference=2),
-		BIOLOGY.2017 = list(
-			sgp.content.areas=c('EARTH_SCIENCE', 'BIOLOGY'),
-			sgp.panel.years=as.character(2016:2017),
-			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.norm.group.preference=3),
+			# sgp.projection.grade.sequences="NO_PROJECTIONS", # Canonical progression
+			sgp.norm.group.preference=2),
+
 	# VIA CHEMISTRY
 		BIOLOGY.2017 = list(
 			sgp.content.areas=c('CHEMISTRY', 'BIOLOGY'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.norm.group.preference=1,
+			sgp.norm.group.preference=3,
 			sgp.projection.sequence = "CHEM_BIO"),
+
 	# VIA PHYSICS
 		BIOLOGY.2017 = list(
 			sgp.content.areas=c('PHYSICS', 'BIOLOGY'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
-			sgp.norm.group.preference=1,
+			sgp.norm.group.preference=4,
 			sgp.projection.sequence = "PHYS_BIO")
 	)
+
 #############################################################
 	CHEMISTRY_2017.config <- list(
 	# VIA BIOLOGY
@@ -100,94 +63,98 @@
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(7:8, 'EOCT', 'EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 			sgp.norm.group.preference=1),
 		CHEMISTRY.2017 = list(
 		  sgp.content.areas=c('SCIENCE', 'BIOLOGY', 'CHEMISTRY'),
 		  sgp.panel.years=as.character(2015:2017),
 		  sgp.grade.sequences=list(c(8, 'EOCT', 'EOCT')),
 		  sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 	  	sgp.norm.group.preference=2),
+
 		CHEMISTRY.2017 = list(
 			sgp.content.areas=c('EARTH_SCIENCE', 'BIOLOGY', 'CHEMISTRY'),
 			sgp.panel.years=as.character(2015:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT', 'EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 			sgp.norm.group.preference=3),
-		CHEMISTRY.2017 = list(
+
+		CHEMISTRY.2017 = list( #  Combined cohorts with Science and Earth Sci 2nd priors
 			sgp.content.areas=c('BIOLOGY', 'CHEMISTRY'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
+			# sgp.projection.grade.sequences="NO_PROJECTIONS", # Canonical progression
 			sgp.norm.group.preference=4),
+
 	# VIA PHYSICS
 		CHEMISTRY.2017 = list(
 			sgp.content.areas=c('PHYSICS', 'CHEMISTRY'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
 			sgp.norm.group.preference=1,
 			sgp.projection.sequence = "PHYS_CHEM")
 	)
+
 #############################################################
+
 	PHYSICS_2017.config <- list(
-	# VIA CHEMISTRY
+		# VIA CHEMISTRY
 		PHYSICS.2017 = list(
 			sgp.content.areas=c('SCIENCE', 'BIOLOGY', 'CHEMISTRY', 'PHYSICS'),
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(8, 'EOCT', 'EOCT', 'EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 			sgp.norm.group.preference=1),
 		PHYSICS.2017 = list(
 		  sgp.content.areas=c('EARTH_SCIENCE', 'BIOLOGY', 'CHEMISTRY', 'PHYSICS'),
 		  sgp.panel.years=as.character(2014:2017),
 		  sgp.grade.sequences=list(c('EOCT', 'EOCT', 'EOCT', 'EOCT')),
 		  sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 		  sgp.norm.group.preference=2),
 		PHYSICS.2017 = list(
 			sgp.content.areas=c('BIOLOGY', 'CHEMISTRY', 'PHYSICS'),
 			sgp.panel.years=as.character(2015:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT', 'EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-		sgp.norm.group.preference=3),
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
+			sgp.norm.group.preference=3),
 		PHYSICS.2017 = list(
 		  sgp.content.areas=c('CHEMISTRY', 'PHYSICS'),
 		  sgp.panel.years=as.character(2016:2017),
 		  sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-		  sgp.exact.grade.progression=TRUE,
+			# sgp.projection.grade.sequences="NO_PROJECTIONS", # Canonical progression
 		  sgp.norm.group.preference=4),
+
 	# VIA BIOLOGY
 		PHYSICS.2017 = list(
 			sgp.content.areas=c(rep('SCIENCE', 2), 'BIOLOGY', 'PHYSICS'),
 			sgp.panel.years=as.character(2014:2017),
 			sgp.grade.sequences=list(c(7:8, 'EOCT','EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 			sgp.norm.group.preference=1),
 	  PHYSICS.2017 = list(
-	    sgp.content.areas=c('SCIENCE', 'BIOLOGY', 'PHYSICS'),
-	    sgp.panel.years=as.character(2015:2017),
-	    sgp.grade.sequences=list(c(8, 'EOCT','EOCT')),
-	    sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
-	    sgp.norm.group.preference=2),
+			sgp.content.areas=c('SCIENCE', 'BIOLOGY', 'PHYSICS'),
+			sgp.panel.years=as.character(2015:2017),
+			sgp.grade.sequences=list(c(8, 'EOCT','EOCT')),
+			sgp.exact.grade.progression=TRUE,
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
+			sgp.norm.group.preference=2),
 		PHYSICS.2017 = list(
 			sgp.content.areas=c('EARTH_SCIENCE', 'BIOLOGY', 'PHYSICS'),
 			sgp.panel.years=as.character(2015:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT', 'EOCT')),
 			sgp.exact.grade.progression=TRUE,
-			sgp.projection.grade.sequences="NO_PROJECTIONS", # max.order.for.projection = 1
+			sgp.projection.grade.sequences="NO_PROJECTIONS",
 			sgp.norm.group.preference=3),
 		PHYSICS.2017 = list(
 			sgp.content.areas=c('BIOLOGY', 'PHYSICS'),
 			sgp.panel.years=as.character(2016:2017),
 			sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-			sgp.exact.grade.progression=TRUE,
 			sgp.norm.group.preference=4,
 			sgp.projection.sequence = "BIO_PHYS")
 	)
---
+++ UT_Course_Progressions_2018.R
@@ -0,0 +1,132 @@
+################################################################################
+###                                                                          ###
+###           Identify 2018 Progressions for Utah SAGE Assessments           ###
+###                                                                          ###
+################################################################################
+
+library(SGP)
+library(data.table)
+
+# Load 2017 SGP data and the 2018 formatted data
+load("Data/Utah_SGP_LONG_Data.Rdata")
+load("Data/Utah_Data_LONG_2018.Rdata")
+
+
+##  Identify only students with 2018 scores
+ids <- unique(Utah_Data_LONG_2018[YEAR=="2018", ID])
+Subset_Data_LONG <- rbindlist(list(
+                      Utah_SGP_LONG_Data[ID %in% ids, list(ID, YEAR, CONTENT_AREA, GRADE, SCALE_SCORE, VALID_CASE)],
+                      Utah_Data_LONG_2018[, list(ID, YEAR, CONTENT_AREA, GRADE, SCALE_SCORE, VALID_CASE)]))
+
+Subset_Data_LONG <- Subset_Data_LONG[YEAR > 2012]
+
+##  Create list of 2018 course progressions by content domains.
+ela.prog <- SGP::courseProgressionSGP(Subset_Data_LONG[grepl("ELA", CONTENT_AREA)], lag.direction="BACKWARD", year='2018')
+math.prog<- SGP::courseProgressionSGP(Subset_Data_LONG[grepl("MATHEMATICS|SEC_MATH", CONTENT_AREA)], lag.direction="BACKWARD", year='2018')
+
+
+####
+####     Mathematics
+####
+
+###  Find out which Math related content areas are present in the Spring 17 data
+names(math.prog$BACKWARD[['2018']])
+
+# ...
+
+sum(math.prog$BACKWARD[['2018']]$MATHEMATICS.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="MATHEMATICS.07"]$COUNT)   #  39,201
+sum(math.prog$BACKWARD[['2018']]$MATHEMATICS.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1!="MATHEMATICS.07"]$COUNT)   #   1,341
+sum(math.prog$BACKWARD[['2018']]$MATHEMATICS.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="MATHEMATICS.07" & CONTENT_AREA_by_GRADE_PRIOR_YEAR.2=="MATHEMATICS.06"]$COUNT)   #  36828
+
+###   Secondary Math I (No Course Regression)
+
+SEC_MATH_I <- math.prog$BACKWARD[['2018']]$SEC_MATH_I.EOCT[!grepl("SEC_MATH_II", CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)]
+table(SEC_MATH_I$CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)
+SEC_MATH_I[COUNT > 1000]  #  Major progressions
+
+###   Viable 1 Prior (Spring 17) SEC_MATH_I Progressions
+SEC_MATH_I[, list(Total=sum(COUNT)), keyby="CONTENT_AREA_by_GRADE_PRIOR_YEAR.1"][!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1 Total
+# 1:                     MATHEMATICS.06    80
+# 2:                     MATHEMATICS.07  1710
+# 3:                     MATHEMATICS.08 38024
+# 4:                    SEC_MATH_I.EOCT  1124
+
+###   Viable 2 Prior SEC_MATH_I Progressions
+SEC_MATH_I[!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1) & !is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.2), list(Total=sum(COUNT)), keyby=c("CONTENT_AREA_by_GRADE_PRIOR_YEAR.1", "CONTENT_AREA_by_GRADE_PRIOR_YEAR.2")][Total > 3000]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1 CONTENT_AREA_by_GRADE_PRIOR_YEAR.2 Total
+# 1:                     MATHEMATICS.08                     MATHEMATICS.07 34655
+
+
+###   Secondary Math 2 (No Course Regression)
+
+SEC_MATH_II <- math.prog$BACKWARD[['2018']]$SEC_MATH_II.EOCT[!grepl("SEC_MATH_III", CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)]
+table(SEC_MATH_II$CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)
+SEC_MATH_II[COUNT > 100]  #  Major progressions
+
+###   Viable 1 Prior (Spring 17) SEC_MATH_II Progressions
+SEC_MATH_II[, list(Total=sum(COUNT)), keyby="CONTENT_AREA_by_GRADE_PRIOR_YEAR.1"][!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1 Total
+# 1:                     MATHEMATICS.06     8
+# 2:                     MATHEMATICS.07    56
+# 3:                     MATHEMATICS.08   840
+# 4:                    SEC_MATH_I.EOCT 35234
+# 5:                   SEC_MATH_II.EOCT   365
+
+###   Viable 2 Prior SEC_MATH_II Progressions
+SEC_MATH_II[!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1) & !is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.2), list(Total=sum(COUNT)), keyby=c("CONTENT_AREA_by_GRADE_PRIOR_YEAR.1", "CONTENT_AREA_by_GRADE_PRIOR_YEAR.2")][Total > 3000]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1   CONTENT_AREA_by_GRADE_PRIOR_YEAR.2   Total
+# 1:                    SEC_MATH_I.EOCT                       MATHEMATICS.08   31151
+
+
+###   Secondary Math 3
+
+SEC_MATH_III <- math.prog$BACKWARD[['2018']]$SEC_MATH_III.EOCT
+table(SEC_MATH_III$CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)
+SEC_MATH_III[COUNT > 1000]  #  Major progressions
+
+###   Viable 1 Prior (Spring 17) Progressions
+SEC_MATH_III[, list(Total=sum(COUNT)), keyby="CONTENT_AREA_by_GRADE_PRIOR_YEAR.1"][!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1)]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1   Total
+# 1:                     MATHEMATICS.07       3
+# 2:                     MATHEMATICS.08      59
+# 3:                    SEC_MATH_I.EOCT     687
+# 4:                   SEC_MATH_II.EOCT    7863
+# 5:                  SEC_MATH_III.EOCT      54
+
+###   Viable 2 Prior (Spring 17 + Spring 16) SEC_MATH_I Progressions
+SEC_MATH_III[!is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.1) & !is.na(CONTENT_AREA_by_GRADE_PRIOR_YEAR.2), list(Total=sum(COUNT)), keyby=c("CONTENT_AREA_by_GRADE_PRIOR_YEAR.1", "CONTENT_AREA_by_GRADE_PRIOR_YEAR.2")][Total > 1500]
+#    CONTENT_AREA_by_GRADE_PRIOR_YEAR.1 CONTENT_AREA_by_GRADE_PRIOR_YEAR.2 Total
+# 1:                   SEC_MATH_II.EOCT                    SEC_MATH_I.EOCT  6833
+
+
+
+####
+####     ELA
+####
+
+###  Find out which grades are present in the Fall ELA data
+names(ela.prog$BACKWARD[['2018']])
+
+# ...
+
+sum(ela.prog$BACKWARD[['2018']]$ELA.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.07"]$COUNT)   #  41,955
+sum(ela.prog$BACKWARD[['2018']]$ELA.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1!="ELA.07"]$COUNT)   #      98
+sum(ela.prog$BACKWARD[['2018']]$ELA.08[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.07" & CONTENT_AREA_by_GRADE_PRIOR_YEAR.2=="ELA.06"]$COUNT)   #  39597
+
+sum(ela.prog$BACKWARD[['2018']]$ELA.09[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.08"]$COUNT)   #  40,364
+sum(ela.prog$BACKWARD[['2018']]$ELA.09[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1!="ELA.08"]$COUNT)   #     119
+sum(ela.prog$BACKWARD[['2018']]$ELA.09[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.08" & CONTENT_AREA_by_GRADE_PRIOR_YEAR.2=="ELA.07"]$COUNT)   #  38175
+
+sum(ela.prog$BACKWARD[['2018']]$ELA.10[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.09"]$COUNT)   #  36,632
+sum(ela.prog$BACKWARD[['2018']]$ELA.10[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1!="ELA.09"]$COUNT)   #     308
+sum(ela.prog$BACKWARD[['2018']]$ELA.10[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.09" & CONTENT_AREA_by_GRADE_PRIOR_YEAR.2=="ELA.08"]$COUNT)  #  34305
+
+sum(ela.prog$BACKWARD[['2018']]$ELA.11[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.10"]$COUNT)   #   1,929
+sum(ela.prog$BACKWARD[['2018']]$ELA.11[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1!="ELA.10"]$COUNT)   #      47
+sum(ela.prog$BACKWARD[['2018']]$ELA.11[CONTENT_AREA_by_GRADE_PRIOR_YEAR.1=="ELA.10" & CONTENT_AREA_by_GRADE_PRIOR_YEAR.2=="ELA.09"]$COUNT) # 1711
+
+
+####
+####     SCIENCE
+####
